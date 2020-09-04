5b2f9e5e2594d7e313467548944044dd8c283877
JNickBaughan <JNickBaughan@gmail.com>
2020-09-04 08:06:44 -0400

    add styled components

--- src/components/welcome-card.tsx
+++ src/components/welcome-card.tsx
@@ -1,18 +1,36 @@
 import React, { FunctionComponent } from "react";
+import styled from "styled-components";
 
 type WelcomeProps = {
   title: string;
   paragraph: string;
 };
 
+const Title = styled.h2`
+  font-size: 1.5em;
+  text-align: center;
+  color: green;
+`;
+
+const Paragraph = styled.div`
+  font-size: 1em;
+  text-align: center;
+  color: blue;
+`;
+
+const Wrapper = styled.div`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 export const WelcomeCard: FunctionComponent<WelcomeProps> = ({
   title,
   paragraph,
 }: WelcomeProps) => (
-  <div>
-    <h2>{title}</h2>
-    <p>{paragraph}</p>
-  </div>
+  <Wrapper>
+    <Title>{title}</Title>
+    <Paragraph>{paragraph}</Paragraph>
+  </Wrapper>
 );
 
 export default WelcomeCard;
